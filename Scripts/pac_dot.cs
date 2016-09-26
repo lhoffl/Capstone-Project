a551175e468832d453442f0860d36b84b92c1b0a
Markvanbaren <vanbarem@mail.gvsu.edu>
2016-09-26 12:31:26 -0700

    Add files via upload
    Pac dots and multiplayer testing added. Pac dots are created via a script in maze.

---
+++ Scripts/pac_dot.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pac_dot : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D co)
+    {
+        if (co.name == "pacman")
+        {
+            Destroy(gameObject);
+        }
+        if (co.name == "maze")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
