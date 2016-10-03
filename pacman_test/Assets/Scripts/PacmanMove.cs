b45613e221760656402135c7282b6a28e491a873
Matthew Hoffman <hoffmatt@mail.gvsu.edu>
2016-10-03 14:29:10 -0400

    #32 Close-up camera implemented. Pacman move animation effected.

--- pacman_test/Assets/Scripts/PacmanMove.cs
+++ pacman_test/Assets/Scripts/PacmanMove.cs
@@ -3,47 +3,64 @@ using System.Collections;
 
 public class PacmanMove : MonoBehaviour
 {
-    public float speed = 0.4f;
-    Vector2 dest = Vector2.zero;
+	public float speed = 0.3f;
+	Vector2 dest = Vector2.zero;
+	string current_direction = "right";
 
-    // Use this for initialization
-    void Start()
-    {
-        dest = transform.position;
-    }
+	void Start()
+	{
+		dest = transform.position;
+	}
 
-    void FixedUpdate()
-    {
-        // Move closer to Destination
-        Vector2 p = Vector2.MoveTowards(transform.position, dest, speed);
-        GetComponent<Rigidbody2D>().MovePosition(p);
+	void FixedUpdate()
+	{	
+		Vector2 p = Vector2.MoveTowards(transform.position, dest, speed);
+		GetComponent<Rigidbody2D>().MovePosition(p);
 
-        // Check for Input if not moving
-        if ((Vector2)transform.position == dest)
-        {
-            if (Input.GetKey(KeyCode.UpArrow) && valid(Vector2.up))
-                dest = (Vector2)transform.position + Vector2.up;
-            if (Input.GetKey(KeyCode.RightArrow) && valid(Vector2.right))
-                dest = (Vector2)transform.position + Vector2.right;
-            if (Input.GetKey(KeyCode.DownArrow) && valid(-Vector2.up))
-                dest = (Vector2)transform.position - Vector2.up;
-            if (Input.GetKey(KeyCode.LeftArrow) && valid(-Vector2.right))
-                dest = (Vector2)transform.position - Vector2.right;
-        }
+		// check first for new user input in any direction
+		if (Input.GetKey (KeyCode.UpArrow) && valid (Vector2.up)) {
+			dest = (Vector2)transform.position + Vector2.up;
+			current_direction = "up";
+		}
+		else if (Input.GetKey (KeyCode.RightArrow) && valid (Vector2.right)) {
+			dest = (Vector2)transform.position + Vector2.right;
+			current_direction = "right";
+		}
+		else if (Input.GetKey (KeyCode.DownArrow) && valid (-Vector2.up)){
+			dest = (Vector2)transform.position - Vector2.up;
+			current_direction = "down";
+		}
+		else if (Input.GetKey (KeyCode.LeftArrow) && valid (-Vector2.right)) {
+			dest = (Vector2)transform.position - Vector2.right;
+			current_direction = "left";
+		}
 
-        // Animation Parameters
-        Vector2 dir = dest - (Vector2)transform.position;
-        GetComponent<Animator>().SetFloat("DirX", dir.x);
-        GetComponent<Animator>().SetFloat("DirY", dir.y);
-    }
+		// if no user input is found, continue moving in the last requested direction
+		else {
+			if (current_direction.Equals("up") && valid (Vector2.up))
+				dest = (Vector2)transform.position + Vector2.up;
+			if (current_direction.Equals("right") && valid (Vector2.right))
+				dest = (Vector2)transform.position + Vector2.right;
+			if (current_direction.Equals("down") && valid (-Vector2.up))
+				dest = (Vector2)transform.position - Vector2.up;
+			if (current_direction.Equals("left") && valid (-Vector2.right))
+				dest = (Vector2)transform.position - Vector2.right;
+		}
 
-    bool valid(Vector2 dir)
-    {
-        // Cast Line from 'next to Pac-Man' to 'Pac-Man'
-        Vector2 pos = transform.position;
-        RaycastHit2D hit = Physics2D.Linecast(pos + dir, pos);
-        return (hit.collider == GetComponent<Collider2D>());
-    }
+		Vector2 dir = dest - (Vector2)transform.position;
+		GetComponent<Animator>().SetFloat("DirX", dir.x);
+		GetComponent<Animator>().SetFloat("DirY", dir.y);
+	}
 
+	bool valid(Vector2 dir)
+	{
+		Vector2 pos = transform.position;
+		RaycastHit2D hit = Physics2D.Linecast(pos + dir, pos);
+		return (hit.collider == GetComponent<Collider2D>());
+	}
 
+	public Vector2 position()
+	{
+		return transform.position;
+	}
 }
