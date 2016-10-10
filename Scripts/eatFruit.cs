948d3aa8adacf61d7cbad64e77fbacca2edf8fc1
pankeyj <pankeyj@mail.gvsu.edu>
2016-10-10 16:23:53 -0400

    Add files via upload

---
+++ Scripts/eatFruit.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class eatFruit : MonoBehaviour {
+    public GameObject cherry;
+	// Use this for initialization
+	void Start () {
+        cherry = GameObject.Find("pm_cherry");
+    }
+	
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if(coll.gameObject.name == "pacman")
+        {
+            cherry.SetActive(false);
+        }
+    }
+	
+}
